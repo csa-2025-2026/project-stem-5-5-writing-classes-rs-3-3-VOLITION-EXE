19e4301259c3019c9fed7ba712e86ff42d87f368
Vu Nguyen <vunguyen01202009@gmail.com>
2025-11-06 22:08:11 +0000

    ers

--- Main.java
+++ Main.java
@@ -15,13 +15,13 @@ public class Main
     Firstname = N.nextLine();
     System.out.println("Enter the person's last name:");
     Lastname = N.nextLine();
-
+    
     System.out.println("Enter the person's age:");
     Age = N.nextInt();
 
     System.out.println("Enter the person's social security number:");
     SSN = N.nextInt();
-
+    N.nextLine();
     Person p = new Person(Firstname, Lastname, Age, SSN);
     p.print();
     
@@ -30,6 +30,7 @@ public class Main
     System.out.println("Maximum oven temperature:\n"+O.getMaxTemp());
     System.out.println("Starting temperature of the oven:\n"+O.getCurrentTemp());
     while(isOn){
+      N.nextLine();
       System.out.println(O.toString());
       System.out.println("To preheat the oven enter \"p\", to turn the oven off enter \"o\", to restart enter \"r\", to quit enter \"q\"");
       switch(N.nextLine()){
